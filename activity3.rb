f2aaade0d42c0c890ab29bf523c887446e456937
mspuy3 <mspuy3@gmail.com>
2022-09-22 19:16:05 +0800

    act 3

--- activity3.rb
+++ activity3.rb
@@ -2,18 +2,21 @@ puts "Michael Siemens Uy"
 puts "Ruby Activity September 20 Tuesday"
 
 def gives_square(num)
+
   acc = 0
+
   for i in 1..num
     if (num % i == 0) && ((num / i).floor == i)
       puts "The square root of #{num} is #{i} and -#{i}"
       acc += 1
       break
     end
-    
   end
+
   if acc == 0
     puts "#{num} has no square root"
   end
+
 end
 
 print "Give me an integer to test if it has a square root: "
